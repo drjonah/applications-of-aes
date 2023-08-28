5f82e66b2c4b954812dd783a66c12db9e6529946
Jonah Lysne <jonahboy03@gmail.com>
2023-08-28 14:04:05 -0700

    Updates to convert file and to password manager

--- apps/textfiles/src/convert_file.py
+++ apps/textfiles/src/convert_file.py
@@ -13,35 +13,39 @@ def is_file(file_name: str) -> bool:
 
 def encrypt_file(aes: AES, file_in: str, file_out: str) -> bool:
     """Encrypts a .txt file and writes to a .bin file."""
-    with open(file_in, "r") as FILE_READ:
-        with open(file_out, "wb") as FILE_WRITE:
-            for parse_line in FILE_READ.readlines():
-                new_line = bytes()
-                chunks = load_chunks(parse_line)
+    with open(file_in, "r") as FILE_READ: # reads txt file
+        with open(file_out, "wb") as FILE_WRITE: # writes bin file
+            for parse_line in FILE_READ.readlines(): # iterate through txt file
+                new_line = bytes() # variable that is written to bin file
+                chunks = load_chunks(parse_line) # splits the line into chunks
 
-                for chunk in chunks: new_line += aes.encrypt(chunk)
+                for chunk in chunks: new_line += aes.encrypt(chunk) # encrypts the chunks
 
-                FILE_WRITE.write(new_line)
+                FILE_WRITE.write(new_line) # writes the encrypted line
 
     return True
 
 def decrypt_file(aes: AES, file_in: str, file_out: str) -> bool:
     """Decrypts a .bin file and writes to a .txt file."""
-    with open(file_in, "rb") as FILE_READ:
-        with open(file_out, "w") as FILE_WRITE:
+    with open(file_in, "rb") as FILE_READ: # reads bin file
+        with open(file_out, "w") as FILE_WRITE: # writes txt file
             # loop that reads the binary file and decrypts it on chunks of 16 bytes
             while True:
-                chunk = FILE_READ.read(16)
-                if not chunk:
+                chunk = FILE_READ.read(16) # reads 16 bytes at a time
+                if not chunk: # breaks when there are no more cunks 
                     break
-                FILE_WRITE.write(aes.decrypt(chunk))
+                FILE_WRITE.write(aes.decrypt(chunk)) # writes decrypted chunks to txt file 
 
     return True
 
-def main(encrypt: bool, file: str, file_out=None) -> None:
+def main(file: str, file_out=None) -> None:
     """Main function that handles arguments and produces output"""
-    # optional param to specify existing output location, default is to create a new file
     assert is_file(file), "Specified input file does not exist."
+    assert file[-3:] in ["txt", "bin"], "Incorrect file type. [txt, bin]"
+
+    encrypt = True if file[-3:] == "txt" else False # encrypts if it is a text file, decrypts if it is a bin
+
+    # optional param to specify existing output location, default is to create a new file
     if file_out is None:
         file_out = file[:-4] + "_output." + ("bin" if encrypt else "txt") # creates path for file output
     else:
@@ -58,10 +62,9 @@ def main(encrypt: bool, file: str, file_out=None) -> None:
     else:
         print("Error converting file. Make sure that the file exist.")
 
+
 if __name__ == "__main__":
-    file_text = "apps/textfiles/src/file_text.txt" # text file (input)
-    file_binary = "apps/textfiles/src/file_binary.bin" # text file (input)
-    encrypt = True # true = encrypt file (file_text.txt => file_text_out.bin), false = decrypt file (file_binary.bin => file_binary_out.txt)
-    file = file_text if encrypt else file_binary # logic to decide file
-    # main
-    main(encrypt, file)
+    file = "apps/textfiles/src/file_text.txt" # text file to encrypt
+    # file = "apps/textfiles/src/file_binary.bin" # text file to decrypt
+
+    main(file) # main method
